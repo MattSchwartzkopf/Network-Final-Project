6587646bc1d5a13d1a9b73b3bec370b2e36a15d8
MatthewSchwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-04-12 13:26:48 -0400

    Starter code and README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Network-Final-Project
+### Network-Final-Project ###
+
+How to run:
+-----------
+
+SERVER: Locate to "safe_tls.py", then run the command "python safe_tls.py -s localhost.pem "" 1060"
+
+CLIENT: Locate to "async_client.py", then run the command "python async_client.py"
+
+Overview: This will allow you to connect to a localhost server and send 
+	  messages.
---
+++ async_client.py
@@ -0,0 +1,53 @@
+"""async_client
+
+Champlain College CSI-235, Spring 2018
+Prof. Josh Auerbach
+
+Bare bones example of asynchronously receiving 
+data from server and user input from stdin
+"""
+
+import asyncio
+
+
+class AsyncClient(asyncio.Protocol):
+
+    def data_received(self, data):
+        """simply prints any data that is received"""
+        print("received: ", data)
+        
+        
+
+@asyncio.coroutine
+def handle_user_input(loop):
+    """reads from stdin in separate thread
+    
+    if user inputs 'quit' stops the event loop
+    otherwise just echos user input
+    """
+    while True:
+        message = yield from loop.run_in_executor(None, input, "> ")
+        if message == "quit":
+            loop.stop()
+            return
+        print(message)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    
+    # we only need one client instance
+    client = AsyncClient()
+    
+    # the lambda client serves as a factory that just returns
+    # the client instance we just created
+    coro = loop.create_connection(lambda: client, "localhost", 1060)
+    
+    loop.run_until_complete(coro)
+
+    # Start a task which reads from standard input
+    asyncio.async(handle_user_input(loop))
+
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()   

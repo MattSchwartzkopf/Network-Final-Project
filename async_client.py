5fd038fb6083b35b2035a35cac72c88d01c58844
Matthew Schwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-04-12 20:12:24 -0400

    Client can now send data and the server now receives data.

--- async_client.py
+++ async_client.py
@@ -12,12 +12,28 @@ import asyncio
 
 class AsyncClient(asyncio.Protocol):
 
+    def __init__(self, message, loop):
+            self.message = message
+            self.loop = loop
+
+    def connection_made(self, transport):
+        self.message = input("Name: ")
+        string = (b'User Joined: ' + self.message.encode())
+        #message = handle_user_input(transport)
+        print("message: ", self.message.encode())
+        transport.write(string)
+        print('Data sent: {!r}'.format(self.message))
+        
     def data_received(self, data):
         """simply prints any data that is received"""
         print("received: ", data)
-        
-        
 
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        print('Stop the event loop')
+        self.loop.stop()
+
+        
 @asyncio.coroutine
 def handle_user_input(loop):
     """reads from stdin in separate thread
@@ -26,28 +42,23 @@ def handle_user_input(loop):
     otherwise just echos user input
     """
     while True:
-        message = yield from loop.run_in_executor(None, input, "> ")
+        message = yield from loop.run_in_executor(None, input)
         if message == "quit":
             loop.stop()
             return
         print(message)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    
-    # we only need one client instance
-    client = AsyncClient()
-    
-    # the lambda client serves as a factory that just returns
-    # the client instance we just created
-    coro = loop.create_connection(lambda: client, "localhost", 1060)
-    
-    loop.run_until_complete(coro)
+       loop = asyncio.get_event_loop()
+       message = 'Temp'
+       coro = loop.create_connection(lambda: AsyncClient(message, loop), "localhost", 1060)
+       while True:
+           loop.run_until_complete(coro)
+       
+       try:
+           loop.run_forever()
+       except:
+           loop.close() 
 
-    # Start a task which reads from standard input
-    asyncio.async(handle_user_input(loop))
 
-    try:
-        loop.run_forever()
-    finally:
-        loop.close()   
+

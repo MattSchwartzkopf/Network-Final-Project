16d14e566fea371924ef3ba551e90ceb354014e0
Matthew Schwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-04-26 15:08:23 -0400

    Server now stores message history

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import struct
 USER_LIST = []
 send_back = ''
 MESSAGES = []
+STORED_DATA = []
 
 class AsyncServer(asyncio.Protocol):
     def __init__(self):
@@ -34,28 +35,70 @@ class AsyncServer(asyncio.Protocol):
     def store_chat_history(self, history):
         with open('chat_history.txt', 'a') as file:
             for word in history:
-                print("TESTTTTTT: ", word[6:])
+                print("TESTTTTTT: ", word)
                 file.write(str(word[6:]) + " ")
             file.close()
         
     def load_chat_history(self):
         print("OLD CHAT HISTORY ")
+        i = 0
+        test = []
+        new = []
+        
         with open('chat_history.txt', 'r') as file:
+            # Store all data
             if file.mode == 'r':
-                for line in file:
-                    MESSAGES.append(line)
-                print(MESSAGES)
-        return(MESSAGES)
+                test.append(file.read().splitlines())
                 
+            # Split data into 4
+            r = 3
+            for i in range(len(test[0])):
+                new.append(test[0][i])
+                if i > 0 and i % r == 0 or i == len(test[0]):
+                    MESSAGES.append(new)
+                    new = []
+                    r += 4
+            print(MESSAGES)
+        return(MESSAGES)
+    
+    def send_user_acception(self, message):
+        message2 = self.load_chat_history()
+        MESSAGES = message2
+        
+        # Formats all messages to send
+        #print(message)
+        formatter = json.loads(message[4:])
+        
+        username = formatter['USERNAME'][0:]
+        USER_LIST.append(username)
+        sender = json.dumps({'USERNAME_ACCEPTED' : True, "INFO" : "Welcome", 'USER_LIST' : USER_LIST, 'MESSAGES' : MESSAGES}).encode()
+        length = struct.pack("!I", len(sender))
+        self.transport.write(length)
+        self.transport.write(sender)
+        print("Sender: ", sender)
+        self.count += 1
+        
+    def send_new_message(self, message):
+        # Formats all messages to send
+        formatter = json.loads(message[4:])
+
+        # Send message
+        username = formatter['MESSAGES'][0][0]
+        dest = formatter['MESSAGES'][0][1]
+        timestamp = formatter['MESSAGES'][0][2]
+        message = formatter['MESSAGES'][0][3]
+        MESSAGES = [username, dest, timestamp, message]
+        self.store_chat_history(MESSAGES)
+        sender = json.dumps({'MESSAGES' : [MESSAGES]}).encode()
+        length = struct.pack("!I", len(sender))
+        self.transport.write(length)
+        self.transport.write(sender)
+        
 @asyncio.coroutine
 def handle_conversation(self, message):
-    temp_history = []
     reader, writer = yield from asyncio.open_connection('localhost', 1060, loop=loop)
-    
-    # Formats all messages to send
-    #print(message)
     formatter = json.loads(message[4:])
-    
+        
     if self.count == 0:
         address = writer.get_extra_info('peername')
         print('Accepted connection from {}'.format(address))
@@ -65,28 +108,10 @@ def handle_conversation(self, message):
     
     # Build USERNAME stuff
     if formatter.__contains__('USERNAME') and self.count == 1:
-        MESSAGES = [formatter['USERNAME'][0:], 'ALL', 1234567, 'heyo']
-
-        #MESSAGES = ['matt', 'ALL', 1234567, 'Hello']
-        username = formatter['USERNAME'][0:]
-        USER_LIST.append(username)
-        sender = json.dumps({'USERNAME_ACCEPTED' : True, "INFO" : "Welcome", 'USER_LIST' : USER_LIST, 'MESSAGES' : [MESSAGES]}).encode()
-        length = struct.pack("!I", len(sender))
-        #send = length + sender
-        self.transport.write(length)
-        self.transport.write(sender)
-        self.count += 1
+        self.send_user_acception(message)
 
     if formatter.__contains__('MESSAGES') and self.count > 1:
-        username = formatter['MESSAGES'][0][0]
-        dest = formatter['MESSAGES'][0][1]
-        timestamp = formatter['MESSAGES'][0][2]
-        message = formatter['MESSAGES'][0][3]
-        MESSAGES = [username, dest, timestamp, message]
-        sender = json.dumps({'MESSAGES' : [MESSAGES]}).encode()
-        length = struct.pack("!I", len(sender))
-        self.transport.write(length)
-        self.transport.write(sender)
+        self.send_new_message(message)
 
 if __name__=='__main__':
     loop = asyncio.get_event_loop()
